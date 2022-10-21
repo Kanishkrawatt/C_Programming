6346268705f9ad022616c349e0b48d89cd632c95
Kanishk <rawat27kanishk@gmail.com>
2022-10-21 12:30:41 +0530

    Formated The Repo

--- CBasic/test.c
+++ CBasic/test.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    // int x ;
-    // x = 3/2+3*8/3-3+1.5/3;
-    // printf("%d",10*5/4%3*2-1 );
-    
-    for(;;);
-    printf("k");
+    int main = 3;
+    printf("%d",main);
 }
